be7ee00d376916410ac63353983e79ddaff3a7d7
Raman Malik <raman.malik@kellogg.northwestern.edu>
2021-03-06 12:35:50 -0600

    structured

--- paperclip.js
+++ paperclip.js
@@ -1,6 +1,19 @@
+let db = firebase.firestore()
+
+// Change main event listener from DOMContentLoaded to 
+// firebase.auth().onAuthStateChanged and move code that 
+// shows login UI to only show when signed out
 firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
+    console.log('signed in')
+
+    // Ensure the signed-in user is in the users collection
+    db.collection('users').doc(user.uid).set({
+      name: user.displayName,
+      email: user.email
+    })
+
     console.log('signed in')
 
     // Sign-out button
@@ -10,33 +23,39 @@ firebase.auth().onAuthStateChanged(async function(user) {
     document.querySelector('.sign-out').addEventListener('click', function(event) {
       console.log('sign out clicked')
       firebase.auth().signOut()
-      document.location.href = 'index.html'
+      document.location.href = 'kelloggram.html'
     })
 
     // Listen for the form submit and create/render the new post
     document.querySelector('form').addEventListener('submit', async function(event) {
       event.preventDefault()
-      let postUsername = user.displayName
+      let paperclipGroupName = document.querySelector('#groupName').value
       let postImageUrl = document.querySelector('#image-url').value
-      let response = await fetch('/.netlify/functions/create_post', {
-        method: 'POST',
-        body: JSON.stringify({
-          userId: user.uid,
-          username: postUsername,
-          imageUrl: postImageUrl
-        })
+      let postNumberOfLikes = 0
+      let docRef = await db.collection('posts').add({ 
+        groupname: paperclipGroupName, 
+        imageUrl: postImageUrl, 
+        likes: 0,
+        created: firebase.firestore.FieldValue.serverTimestamp()
       })
-      let post = await response.json()
+      let postId = docRef.id // the newly created document's ID
       document.querySelector('#image-url').value = '' // clear the image url field
-      renderPost(post)
+      document.querySelector('#groupName').value = '' // clear the image url field
+      renderPost(postId, paperclipGroupName, postImageUrl, postNumberOfLikes)
     })
 
-    let response = await fetch('/.netlify/functions/get_posts')
-    let posts = await response.json()
+    // Render all posts when the page is loaded
+    let querySnapshot = await db.collection('posts').orderBy('created').get()
+    let posts = querySnapshot.docs
     for (let i=0; i<posts.length; i++) {
-      let post = posts[i]
-      renderPost(post)
+      let postId = posts[i].id
+      let postData = posts[i].data()
+      let paperclipGroupName = postData.groupname
+      let postImageUrl = postData.imageUrl
+      let postNumberOfLikes = postData.likes
+      renderPost(postId, paperclipGroupName, postImageUrl, postNumberOfLikes)
     }
+
   } else {
     // Signed out
     console.log('signed out')
@@ -52,7 +71,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
       signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID
       ],
-      signInSuccessUrl: 'index.html'
+      signInSuccessUrl: 'kelloggram.html'
     }
 
     // Starts FirebaseUI Auth
@@ -60,119 +79,31 @@ firebase.auth().onAuthStateChanged(async function(user) {
   }
 })
 
-// given a single post Object, render the HTML and attach event listeners
-// expects an Object that looks similar to:
-// {
-//   id: 'abcdefg',
-//   username: 'brian',
-//   imageUrl: 'https://images.unsplash.com/...',
-//   likes: 12,
-//   comments: [
-//     { username: 'brian', text: 'i love tacos!' },
-//     { username: 'ben', text: 'fake news' }
-//   ]
-// }
-async function renderPost(post) {
-  let postId = post.id
+async function renderPost(postId, paperclipGroupName, postImageUrl, postNumberOfLikes) {
   document.querySelector('.posts').insertAdjacentHTML('beforeend', `
-    <div class="post-${postId} md:mt-16 mt-8 space-y-8">
+    <div class="post-${postId} md:mt-16 mt-8 space-y-4">
       <div class="md:mx-0 mx-4">
-        <span class="font-bold text-xl">${post.username}</span>
+        <span class="font-bold text-xl">${paperclipGroupName}</span>
       </div>
+  
       <div>
-        <img src="${post.imageUrl}" class="w-full">
+        <img src="${postImageUrl}" class="w-full">
       </div>
+  
       <div class="text-3xl md:mx-0 mx-4">
-        <button class="like-button">❤️</button>
-        <span class="likes">${post.likes}</span>
-      </div>
-      <div class="comments text-sm md:mx-0 mx-4 space-y-2">
-        ${renderComments(post.comments)}
-      </div>
-      <div class="w-full md:mx-0 mx-4">
-        ${renderCommentForm()}
+        <button class="like-button">📎</button>
+        <span class="likes">${postNumberOfLikes}</span>
       </div>
     </div>
   `)
-
-  // listen for the like button on this post
-  let likeButton = document.querySelector(`.post-${postId} .like-button`)
-  likeButton.addEventListener('click', async function(event) {
+  document.querySelector(`.post-${postId} .like-button`).addEventListener('click', async function(event) {
     event.preventDefault()
     console.log(`post ${postId} like button clicked!`)
-    let currentUserId = firebase.auth().currentUser.uid
-
-    let response = await fetch('/.netlify/functions/like', {
-      method: 'POST',
-      body: JSON.stringify({
-        postId: postId,
-        userId: currentUserId
-      })
+    let existingNumberOfLikes = document.querySelector(`.post-${postId} .likes`).innerHTML
+    let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
+    document.querySelector(`.post-${postId} .likes`).innerHTML = newNumberOfLikes
+    await db.collection('posts').doc(postId).update({
+      likes: firebase.firestore.FieldValue.increment(1)
     })
-    if (response.ok) {
-      let existingNumberOfLikes = document.querySelector(`.post-${postId} .likes`).innerHTML
-      let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
-      document.querySelector(`.post-${postId} .likes`).innerHTML = newNumberOfLikes
-    }
   })
-
-  // listen for the post comment button on this post
-  let postCommentButton = document.querySelector(`.post-${postId} .post-comment-button`)
-  postCommentButton.addEventListener('click', async function(event) {
-    event.preventDefault()
-    console.log(`post ${postId} post comment button clicked!`)
-
-    // get the text of the comment
-    let postCommentInput = document.querySelector(`.post-${postId} input`)
-    let newCommentText = postCommentInput.value
-    console.log(`comment: ${newCommentText}`)
-
-    // create a new Object to hold the comment's data
-    let newComment = {
-      postId: postId,
-      username: firebase.auth().currentUser.displayName,
-      text: newCommentText
-    }
-
-    // call our back-end lambda using the new comment's data
-    await fetch('/.netlify/functions/create_comment', {
-      method: 'POST',
-      body: JSON.stringify(newComment)
-    })
-
-    // insert the new comment into the DOM, in the div with the class name "comments", for this post
-    let commentsElement = document.querySelector(`.post-${postId} .comments`)
-    commentsElement.insertAdjacentHTML('beforeend', renderComment(newComment))
-
-    // clears the comment input
-    postCommentInput.value = ''
-  })
-}
-
-// given an Array of comment Objects, loop and return the HTML for the comments
-function renderComments(comments) {
-  if (comments) {
-    let markup = ''
-    for (let i = 0; i < comments.length; i++) {
-      markup += renderComment(comments[i])
-    }
-    return markup
-  } else {
-    return ''
-  }
-}
-
-// return the HTML for one comment, given a single comment Object
-function renderComment(comment) {
-  return `<div><strong>${comment.username}</strong> ${comment.text}</div>`
-}
-
-// return the HTML for the new comment form
-function renderCommentForm() {
-  let commentForm = ''
-  commentForm = `
-    <input type="text" class="mr-2 rounded-lg border px-3 py-2 focus:outline-none focus:ring-purple-500 focus:border-purple-500" placeholder="Add a comment...">
-    <button class="post-comment-button py-2 px-4 rounded-md shadow-sm font-medium text-white bg-purple-600 focus:outline-none">Post</button>
-  `
-  return commentForm
 }
