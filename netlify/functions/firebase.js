9ed91abb69d379b9c442b1bb73acc58cbd76e58b
Raman Malik <raman.malik@kellogg.northwestern.edu>
2021-03-13 14:28:14 -0600

    adding config

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyAifIXtUqXHGO6I1PCQrNR_f4fAmlx2kCU",
+  authDomain: "paperclip---kiei451.firebaseapp.com",
+  projectId: "paperclip---kiei451",
+  storageBucket: "paperclip---kiei451.appspot.com",
+  messagingSenderId: "601420250611",
+  appId: "1:601420250611:web:daa06d99774bd5c1c4e686"
+};
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
