2ec4532d0ce735a08d37755ccdcc593e5fe1ed29
Raman Malik <raman.malik@kellogg.northwestern.edu>
2021-03-14 20:23:11 -0500

    add conditional logic to get_groups lamdba function

--- netlify/functions/get_groups.js
+++ netlify/functions/get_groups.js
@@ -2,31 +2,60 @@ let firebase = require('./firebase')
 
 exports.handler = async function(event) {
 console.log('hello from the back-end')
-console.log(event)
-// let queryStringUserId = event.queryStringParameter.userId
-// console.log(queryStringUserId)
 
-let groupsAPI = []
 let db = firebase.firestore()
-let querySnapshot = await db.collection('groups')
-                            .orderBy('created')
-                            .get()
-    let groups = querySnapshot.docs
-    for (let i=0; i<groups.length; i++) {
-      let groupId = groups[i].id
-      let groupData = groups[i].data()
-      let groupName = groupData.groupname
-      let groupImageUrl = groupData.imageUrl
-      groupsAPI.push({
-        groupName: groupName,
-        groupImageUrl: groupImageUrl,
-        groupId: groupId
-      })
-    }
 
-  // return an Object in the format that a Netlify lambda function expects
+if (event.queryStringParameters.groupid) { 
+
+  console.log('has groupId')
+
+  let groupsAPI = []
+  let queryStringGroupId = event.queryStringParameters.groupid
+  console.log(queryStringGroupId)
+  let querySnapshot = await db.collection('groups')
+                              .doc(queryStringGroupId)
+                              .get()
+  let groupData = querySnapshot.data()
+  let groupName = groupData.groupname
+  let groupImageUrl = groupData.imageUrl
+
+  groupsAPI.push({
+      groupName: groupName,
+      groupImageUrl: groupImageUrl,
+      groupId: queryStringGroupId
+      })
+
   return {
     statusCode: 200,
     body: JSON.stringify(groupsAPI)
   }
+} else { 
+
+  console.log('no groupId')
+
+  let groupsAPI = []
+  let querySnapshot = await db.collection('groups')
+                              .orderBy('created')
+                              .get()
+  let groups = querySnapshot.docs
+
+  for (let i=0; i<groups.length; i++) {
+       let groupId = groups[i].id
+       let groupData = groups[i].data()
+       let groupName = groupData.groupname
+       let groupImageUrl = groupData.imageUrl
+  
+  groupsAPI.push({
+      groupName: groupName,
+      groupImageUrl: groupImageUrl,
+      groupId: groupId
+  })
+  }
+
+    // return an Object in the format that a Netlify lambda function expects
+    return {
+      statusCode: 200,
+      body: JSON.stringify(groupsAPI)
+    }
+}
 }
